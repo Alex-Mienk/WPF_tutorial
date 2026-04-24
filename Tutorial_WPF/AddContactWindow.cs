7e73445561ceac7d3c9d9627797c276253c8baa4
Alex-Mienk <mienko.alex@gmail.com>
2026-04-24 15:57:40 +0200

    Commit of the tutorial

---
+++ Tutorial_WPF/AddContactWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tutorial_WPF
+{
+    public partial class AddContactWindow : Window
+    {
+        public Contact NewContact { get; private set; }
+        public AddContactWindow()
+        {
+            InitializeComponent();
+            NewContact = new Contact();
+            DataContext = NewContact;
+        }
+    }
+}
---
+++ Tutorial_WPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tutorial_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public partial class AddContactWindow : Window
+        {
+            public Contact NewContact { get; private set; }
+            public AddContactWindow()
+            {
+                InitializeComponent();
+                NewContact = new Contact();
+                DataContext = NewContact;
+            }
+        }
+
+        private ObservableCollection<Contact> Contacts {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Contacts = new ObservableCollection<Contact>();
+
+            DataContext = Contacts;
+        }
+    }
+
+    public class Contact{
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public Gender Gender { get; set; }
+        public Contact()
+        {
+        }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    
+
+}
